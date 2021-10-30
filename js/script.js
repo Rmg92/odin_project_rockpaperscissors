223f4155a837672573ed1b3547374b0268d039a2
Ricardo <ricardomg92@hotmail.com>
2021-10-30 12:00:44 +0200

    added the code for the player selection

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 /* Computer choice is stored */
 const computerSelection = computerPlay();
 /* Player choice is stored */
+const playerSelection = playerPlay();
 
 /* Computer makes a random choice between Rock, Paper and Scissor */
 function computerPlay() {
-    let computerSelection = Math.round((Math.random()*100)+1);
-    console.log(computerSelection);
+    /* a random number between 1 and 100 is chosen*/
+    let computerSelection = Math.round((Math.random() * 100) + 1);
+
     if (computerSelection < 34) {
-        return "Rock";
+        /* if number is lower than 34, computer choses Rock*/
+        return "rock";
     } else if (computerSelection > 66) {
-        return "Paper"
+        /* if number is higher than 66, computer choses Paper*/
+        return "paper"
     } else {
-        return "Scissor"
+        /* if number is between 33 and 66, computer choses Scissor*/
+        return "scissor"
     }
 }
+
 /* User makes a choice between Rock, Paper and Scissor */
+function playerPlay() {
+    let playerSelection = prompt("Select rock, paper or scissor!")
+    return playerSelection.toLowerCase();
+}
+
+/* One round of the game is played, computer vs player */
+function playRound(playerSelection, computerSelection) {
+    
+}
 
 /* Game is played, it gets both computer and user choices and compares them to determine the winner, first to have 5 wins, wins the game */
