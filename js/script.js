ec95dacf5069ab74bbe933fecdf63bd5261f3ec0
Ricardo <ricardomg92@hotmail.com>
2021-11-22 21:57:01 +0100

    Changed the code so the first to reach five wins, wins the game. Also some improvements to the code in general.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-let roundCounter = 1;
-
 
 const playerChoice = document.querySelector('#playerSelection');
 const computerChoice = document.querySelector('#computerSelection');
-const round = document.querySelector('#roundCounter');
-const score = document.querySelector('#score')
+const score = document.querySelector('#score');
+const roundResult = document.querySelector('#roundResult');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -22,94 +20,74 @@ function computerPlay() {
 
     if (computerSelection < 34) {
         /* if number is lower than 34, computer choses Rock*/
-        return "Rock";
+        return "rock";
     } else if (computerSelection > 66) {
         /* if number is higher than 66, computer choses Paper*/
-        return "Paper"
+        return "paper"
     } else {
         /* if number is between 33 and 66, computer choses Scissor*/
-        return "Scissor"
+        return "scissor"
     }
+}
+
+function playerWin(playerSelection, computerSelection) {
+    roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+    playerScore++;
+    score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
+    game();
+}
+
+function computerWin(playerSelection, computerSelection) {
+    roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
+    computerScore++;
+    score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
+    game();
+}
+
+function newGame() {
+    computerScore = 0;
+    playerScore = 0;
+    score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
 }
 
 /* One round of the game is played, computer vs player */
 function playRound(playerSelection, computerSelection) {
-
     playerChoice.textContent = "Player Choice: " + playerSelection;
-    computerChoice.textContent = "Computer Choice: " + computerSelection;
+    computerChoice.textContent = "Computer Choice: " + computerSelection; 
 
-    const roundResult = document.querySelector('#roundResult');
-
-    let winner = "";
     if (playerSelection === computerSelection) {
         roundResult.textContent = 'Tie!';
-        winner = "tie";
-    } else if (playerSelection == "Rock") { //if player choses rock it compares it to the computer choice and sets the winner
-        if (computerSelection == "Paper") {
-            roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
-            winner = "computer";
-            roundCounter++;
-            computerScore++;
+    } else if (playerSelection == "rock") { //if player choses rock it compares it to the computer choice and sets the winner
+        if (computerSelection == "paper") {
+            computerWin(playerSelection, computerSelection);
         } else {
-            roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
-            winner = "user";
-            roundCounter++;
-            playerScore++;
+            playerWin(playerSelection, computerSelection);
         }
-    } else if (playerSelection == "Paper") { //if player choses paper it compares it to the computer choice and sets the winner
-        if (computerSelection == "Scissor") {
-            roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
-            winner = "computer";
-            roundCounter++;
-            computerScore++;
+    } else if (playerSelection == "paper") { //if player choses paper it compares it to the computer choice and sets the winner
+        if (computerSelection == "scissor") {
+            computerWin(playerSelection, computerSelection);
         } else {
-            roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
-            winner = "user";
-            roundCounter++;
-            playerScore++;
+            playerWin(playerSelection, computerSelection);
         }
-    } else if (playerSelection == "Scissor") { //if player choses scissor it compares it to the computer choice and sets the winner
-        if (computerSelection == "Rock") {
-            roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
-            winner = "computer";
-            roundCounter++;
-            computerScore++;
+    } else if (playerSelection == "scissor") { //if player choses scissor it compares it to the computer choice and sets the winner
+        if (computerSelection == "rock") {
+            computerWin(playerSelection, computerSelection);
         } else {
-            roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
-            winner = "user";
-            roundCounter++;
-            playerScore++;
+            playerWin(playerSelection, computerSelection);
         }
     }
-    game(winner);
 }
 
 /* Game is played, it plays a round of the game, increments the score of the winner and at the end of 5 rounds the player with highest score wins the game.
  If tie or wrong choice by user -> repeat round */
-function game(roundWinner) {
-    if (roundCounter < 5) {
-        if (roundWinner === "computer") {
-            score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
-            round.textContent = 'Round: ' + roundCounter;
-        } else if (roundWinner === "user") {
-            score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
-            round.textContent = 'Round: ' + roundCounter;
-        } else {
-            score.textContent = "Player Score: " + playerScore + " Computer Score: " + computerScore;
-        }
-    } else {
-        if (playerScore > computerScore) {
-            alert('Final Score - Player: ' + playerScore + ' Computer Score: ' + computerScore);
-            alert("User wins the game! Press Ok to start a New Game");
-            roundCounter = 0;
-            computerScore = 0;
-            playerScore = 0
-        } else {
-            alert('Final Score - Player: ' + playerScore + ' Computer Score: ' + computerScore);
-            alert("Computer wins the game! Press Ok to start a New Game");
-            roundCounter = 0;
-            computerScore = 0;
-            playerScore = 0
-        }
+function game() {
+    if (playerScore === 5) {
+        alert('Final Score - Player: ' + playerScore + ' Computer Score: ' + computerScore);
+        alert("You win the game! Press Ok to start a New Game");
+        newGame();
+    } else if (computerScore === 5) {
+        alert('Final Score - Player: ' + playerScore + ' Computer Score: ' + computerScore);
+        alert("Computer wins the game! Press Ok to start a New Game");
+        newGame();
     }
 }
